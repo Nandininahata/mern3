110ef7f43a692428287881bbf06b88dea77ecd6e
Nandininahata <96577332+Nandininahata@users.noreply.github.com>
2023-08-28 22:03:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -423,17 +423,23 @@ const foodoriginaldata= [{
 
     }
  }
-console.log("Function that shows all the Food items with Protien high to low:")
-function itemProtienHigh2Low(){
-  let a = [...foodoriginaldata]
+ function itemProtienHigh2Low(){
+  let a = [...foodOriginalData]
   a.sort((p1,p2)=> p2.protiens - p1.protiens)
   console.table(a)
 }
 itemProtienHigh2Low()
+
+console.log("Function that shows all the Food items with Cal low to high:")
+
+
 console.log("Function that shows all the Food items with Cal low to high:")
 function itemCabLow2High(){
-  let a = [...foodoriginaldata];
+  let a = [...foodOriginalData];
   a.sort((p1,p2)=> p1.cab - p2.cab)
   console.table(a)
 }
-itemCabLow2High()
+  function getitemcabLow2High(){
+    foodoriginaldata.sort((c1,c2)=> c1.cab-c2.cab)
+    console.table(foodoriginaldata)
+  }
